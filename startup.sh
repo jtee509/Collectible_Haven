adb08b4b0f9374e01fc12ad3f538aeaf5d8fc15e
jtee509 <jtee509@gmail.com>
2025-04-20 21:30:27 +0800

    attemp_install

--- startup.sh
+++ startup.sh
@@ -6,12 +6,17 @@ sudo apt upgrade -y
 
 # Install PHP 8.3 and required extensions
 sudo apt install -y software-properties-common
-sudo add-apt-repository ppa:ondrej/php
+
 sudo apt update
-sudo apt install -y php8.3 php8.3-cli php8.3-fpm php8.3-mysql php8.3-xml php8.3-mbstring php8.3-curl php8.3-zip
+sudo apt install -y php php-cli php-fpm php-mysql php-xml php-mbstring php-curl php-zip php-bcmath
 
 # Install Composer
-curl -sS https://getcomposer.org/installer | php8.3 -- --install-dir=/usr/local/bin --filename=composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'.PHP_EOL; } else { echo 'Installer corrupt'.PHP_EOL; unlink('composer-setup.php'); exit(1); }"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+s
+sudo mv composer.phar /usr/local/bin/composer
 
 # Install MariaDB
 sudo apt install -y mariadb-server
