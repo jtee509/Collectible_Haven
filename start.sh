23f59c15eb84c0b30ca828293803c5f54aeca7ec
jtee509 <jtee509@gmail.com>
2025-04-20 19:56:13 +0800

    fix_dependency_issue

--- start.sh
+++ start.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
-# Start services
+# Start MySQL properly (fixes "unrecognized service" error)
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mysql_install_db --user=mysql --ldata=/var/lib/mysql
+fi
+
 service mysql start
-service php8.3-fpm start
+
+# Wait for MySQL to be ready
+while ! mysqladmin ping -hlocalhost --silent; do
+    sleep 1
+done
+
+# Configure database
+mysql -e "CREATE DATABASE IF NOT EXISTS collectible_haven;" \
+      -e "CREATE USER IF NOT EXISTS 'collectible_user'@'localhost' IDENTIFIED BY 'password';" \
+      -e "GRANT ALL PRIVILEGES ON collectible_haven.* TO 'collectible_user'@'localhost';" \
+      -e "FLUSH PRIVILEGES;"
+
+# Run migrations
+cd /var/www/collectible_haven
+php artisan migrate --force
+
+# Start cron
 service cron start
 
-# Configure MySQL
-mysql -e "CREATE DATABASE IF NOT EXISTS collectible_haven;"
-mysql -e "CREATE USER IF NOT EXISTS 'collectible_user'@'localhost' IDENTIFIED BY 'test';"
-mysql -e "GRANT ALL PRIVILEGES ON collectible_haven.* TO 'collectible_user'@'localhost';"
-mysql -e "FLUSH PRIVILEGES;"
+# Start Vite dev server in background
+npm run dev &
 
-# Start npm run dev
-cd /var/www/collectible_haven
-npm run dev &  # Run in background
-
-# Start nginx (this will keep the container running)
-exec nginx -g "daemon off;"
+# Start Laravel development server
+php artisan serve --host=0.0.0.0 --port=8000
