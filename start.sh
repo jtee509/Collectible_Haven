1a7a7be5560f6d12163e816c97226203ec530f37
jtee509 <jtee509@gmail.com>
2025-04-20 19:46:29 +0800

    docker-configuration-fix

--- start.sh
+++ start.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# Start MariaDB
+# Start MySQL
 service mysql start
 
-# Start PHP-FPM
-service php8.3-fpm start
+# Configure database
+mysql -e "CREATE DATABASE IF NOT EXISTS collectible_haven;"
+mysql -e "CREATE USER IF NOT EXISTS 'collectible_user'@'localhost' IDENTIFIED BY 'test';"
+mysql -e "GRANT ALL PRIVILEGES ON collectible_haven.* TO 'collectible_user'@'localhost';"
+mysql -e "FLUSH PRIVILEGES;"
 
-# Start cron
-service cron start
+# Run migrations (optional)
+cd /var/www/collectible_haven
+php artisan migrate --force
 
-# Start nginx
-nginx -g "daemon off;"
+# Start Laravel development server
+php artisan serve --host=0.0.0.0 --port=8000
